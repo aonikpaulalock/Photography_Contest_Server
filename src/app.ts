cad2c940b11e7d21e705ccfaa89171f1b20a58ca
aonikpaulalock <alockpaul9845@gmail.com>
2024-11-20 22:10:04 +0600

    auth validation updated

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,10 @@ const app: Application = express();
 // perser
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}));
 
 // Use router
 app.use("/api", router)
